8ff5d36eef829ca5c6628049e7d9770be3eaf032
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2024-03-17 23:05:23 -0400

    Add GH Pages Data

--- src/_includes/js/url_fix.js
+++
@@ -1,8 +0,0 @@
-const repoURL = '/comm429-framework-app-v2'
-const githubURL = 'https://georgecrochiere.github.io'
-document.querySelectorAll('a').forEach(url => {
-    if (url.href.startsWith(githubURL) && !url.href.includes(repoURL)) {
-        url.href = githubURL + repoURL + url.href.substring(githubURL.length);
-        console.log(url.href);
-    }
-});
