69798912e6825efa644f09080ba38ff344c9f90e
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2024-03-17 23:03:32 -0400

    Fixed Pagination format and fixed urls (hopegully)
    Added additional layout and reformatted how workout pages were built to allow for navigation on GH Pages.

--- _site/src/js/url_fix.js
+++ _site/src/js/url_fix.js
@@ -1,9 +1,8 @@
-if (window.location.href.startsWith('http://localhost')) {
-    const repoURL = '/comm429-framework-app-v2'
-    const githubURL = 'https://georgecrochiere.github.io'
-    document.querySelectorAll('a').forEach(url => {
-        if (url.href.startsWith(githubURL)) {
-            url.href = githubURL + repoURL + url.href.substrig(githubURL.length);
-        }
-    });
-}
+const repoURL = '/comm429-framework-app-v2'
+const githubURL = 'https://georgecrochiere.github.io'
+document.querySelectorAll('a').forEach(url => {
+    if (url.href.startsWith(githubURL) && !url.href.includes(repoURL)) {
+        url.href = githubURL + repoURL + url.href.substring(githubURL.length);
+        console.log(url.href);
+    }
+});
